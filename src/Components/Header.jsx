51bb27c455c2f301466e5523307b01e7d5038d25
subham8391 <subhamdas967990@gmail.com>
2023-10-27 17:11:21 +0530

    new commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ProductDetails from './ProductDetails';
 import { headerContent } from '../data'
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setIsModalOpen(false);
+    };
   return (
     <div className="header-section">
       <div className="header-container">
         {headerContent.map((headding, index) => (
           <h1 key={index}>{headding}</h1>
         ))}
-        <button className='hq-view'>Quick View</button>
+        <button className='hq-view' onClick={openModal}>Quick View</button>
       </div>
+      <ProductDetails isOpen={isModalOpen} closeModal={closeModal}/>
     </div>
 
   )
